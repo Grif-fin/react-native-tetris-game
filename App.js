5d7d80bc99df8411d31ca8067c09e9fed46cf403
Hani <hanytn@gmail.com>
2018-05-26 19:09:41 +0100

    Playing around with react-native-game-engine

--- App.js
+++ App.js
@@ -1,28 +1,40 @@
 import React, { PureComponent } from "react";
-import { AppRegistry, StyleSheet, StatusBar } from "react-native";
-import { GameEngine } from "react-native-game-engine";
-import { Finger } from "./renderers";
-import { MoveFinger } from "./systems"
+import { AppRegistry, StyleSheet, Dimensions, View } from "react-native";
+import { GameEngine, GameLoop } from "react-native-game-engine";
+import {Tile} from "./app/tile.js"
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+const RADIUS = 25;
 
 export default class BestGameEver extends PureComponent {
   constructor() {
     super();
+    this.state = {
+      x: WIDTH / 2 - RADIUS,
+      y: HEIGHT / 2 - RADIUS
+    };
   }
+
+  updateHandler = ({ touches, screen, time }) => {
+    let move = touches.find(x => x.type === "move");
+    if (move) {
+      this.setState({
+        x: this.state.x + move.delta.pageX,
+        y: this.state.y + move.delta.pageY
+      });
+    }
+  };
 
   render() {
     return (
       <GameEngine 
         style={styles.container} 
-        systems={[MoveFinger]}
+        systems={[]}
         entities={{ 
-          1: { position: [40,  200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
-          2: { position: [100, 200], renderer: <Finger />}, //-- and a renderer property (optional). If no renderer
-          3: { position: [160, 200], renderer: <Finger />}, //-- is supplied with the entity - it won't get displayed.
-          4: { position: [220, 200], renderer: <Finger />}, 
-          5: { position: [280, 200], renderer: <Finger />}
+          1: { position: [40,  200], renderer: <Tile />}, //-- Notice that each entity has a unique id (required)
+          2: { position: [100, 200], renderer: <Tile />}
         }}>
 
-        <StatusBar hidden={true} />
 
       </GameEngine>
     );
@@ -33,6 +45,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFF"
+  },
+  player: {
+    position: "absolute",
+    backgroundColor: "pink",
+    width: RADIUS * 2,
+    height: RADIUS * 2
   }
 });
 
--- app/tile.js
+++ app/tile.js
@@ -3,26 +3,25 @@ import { StyleSheet, View } from "react-native";
 
 const RADIUS = 20;
 
-class Finger extends PureComponent {
+class Tile extends PureComponent {
   render() {
     const x = this.props.position[0] - RADIUS / 2;
     const y = this.props.position[1] - RADIUS / 2;
     return (
-      <View style={[styles.finger, { left: x, top: y }]} />
+      <View style={[styles.tile, { left: x, top: y }]} />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  finger: {
-    borderColor: "#CCC",
+  tile: {
+    borderColor: "black",
     borderWidth: 4,
-    borderRadius: RADIUS * 2,
     width: RADIUS * 2,
     height: RADIUS * 2,
-    backgroundColor: "pink",
+    backgroundColor: "white",
     position: "absolute"
   }
 });
 
-export { Finger };
+export { Tile };
